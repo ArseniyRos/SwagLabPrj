67c539af1257ded39dcba5a51e5a33d0314cdaca
Arseniy <arseniy.rosenberg@gmail.com>
2022-01-19 21:40:04 +0200

    Added Before and AfterClass

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -1,4 +1,30 @@
 package tests;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeTest;
+import pageObjects.Urls;
+
 public class BaseTest {
+
+    WebDriver driver;
+
+    @BeforeClass
+    public void setup(){
+
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get(Urls.BASE_URL);
+
+    }
+
+    @AfterClass
+    public void tearDown(){
+        driver.quit();
+    }
+
 }
