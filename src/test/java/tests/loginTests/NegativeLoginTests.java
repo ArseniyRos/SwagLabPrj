0bd2f6cf2112cccf094912115968e6a2f8daf7c9
Arseniy <arseniy.rosenberg@gmail.com>
2022-01-23 22:11:49 +0200

    added positive login tests, start write negative tests

--- src/test/java/tests/loginTests/NegativeLoginTests.java
+++ src/test/java/tests/loginTests/NegativeLoginTests.java
@@ -1,9 +1,20 @@
 package tests.loginTests;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import pageObjects.Credentials;
+import pageObjects.LoginPage;
+import pageObjects.ProductsPage;
+import pageObjects.Urls;
 import tests.BaseTest;
 
 public class NegativeLoginTests extends BaseTest {
 
-
+    @Test(testName = "Login with locked user")
+    public void test_01(){
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.fillForm(Credentials.LOCKED_USER, Credentials.CORRECT_PASSWORD);
+        Assert.assertEquals(loginPage.getErrorMessage(),"Epic sadface: Sorry, this user has been locked out.");
+    }
 
 }
